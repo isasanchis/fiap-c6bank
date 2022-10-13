55f1f3cf42aaff7cae4a507c531bba2ac5a0547f
Isabella Sanchis <isasanchis3@gmail.com>
2022-10-13 11:53:30 -0300

    Criando tela de login

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,69 @@
 import React from 'react'
+import {Typography} from '@mui/material'
+import Link from 'next/link'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+type CopyProps = {
+  site: string;
+  sx: object;
+}
+
+function Copyright(props: CopyProps) {
+  return(
+    <Typography>
+      {'Copyright ©️'}
+      <Link color="inherit" href="https://avanade.com.br">
+        {props.site}
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  )
+}
+
+const theme = createTheme();
 
 export default function LoginPage() {
   return (
-    <div>LoginPage</div>
+    <>
+      <ThemeProvider theme={theme}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+            <Box sx={{mt:8, display:'flex',flexDirection:'column', alignItems:'center'}}>
+              <Typography component="h1" variant="h5">
+                Login 
+              </Typography>
+              <Box component="form" onSubmit={(e) => console.log('Enviou')}>
+                <TextField 
+                  required 
+                  fullWidth 
+                  margin="normal"
+                  id="email" 
+                  label="Digite o e-mail" 
+                  name="email" 
+                  autoComplete="email" 
+                  autoFocus 
+                />
+                <TextField 
+                  required 
+                  fullWidth 
+                  margin="normal"
+                  id="password" 
+                  type="password" 
+                  label="Digite a senha" 
+                  name="password" 
+                  autoComplete="current-password" 
+                  autoFocus 
+                />
+              </Box>
+            </Box>
+          <Copyright site=" www.avanade.com.br" sx={{mt:8, mb:4}} />
+        </Container>
+      </ThemeProvider>
+    </>
   )
 }
